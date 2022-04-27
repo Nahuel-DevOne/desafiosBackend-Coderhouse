4f189be773b065b74d4102100da00b61ae05c080
Nahuel-DevOne <nahuel.profesor@gmail.com>
2022-04-27 20:34:13 -0300

    terminando con Handlebars

--- desafio5/Handlebars/index.js
+++ desafio5/Handlebars/index.js
@@ -1,25 +1,41 @@
 const fs = require("fs");
 const express = require('express');
+var handlebars = require("express-handlebars").create({
+    defaultLayout: "main",
+  });
 const app = express();
 const Container = require("./container");
 const path = "./products.json";
 const container = new Container(path);
 const PORT = 8080;
+// const { engine } = require('express-handlebars');
+
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-//para usar el motor de plantilla pug
-app.set("view engine", "pug");
+/** para usar el motor de plantilla Handlebars */
+// app.engine("hbs", engine({  
+//     extname: ".hbs",
+//     defaultLayout: "main.hbs",
+//     partialsDir: __dirname + "views/partials",
+// }));
+
+// app.set("view engine", "hbs");
+// app.set("views", "./views");
+
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
 
 //para renderizar el formulario de adicionar producto
 app.get("/", (req, res) => {
-  res.render("formAddProduct.pug");
+  res.render("formAddProduct.hbs");
 });
 
 //para renderizar la vista de todos los productos
 app.get("/products", async (req, res) => {
-  res.render("index.pug", { products: await container.getAll() });
+  res.render("index.hbs", { products: await container.getAll() });
 });
 
 //para guardar el producto del formulario de adicionar producto
@@ -30,7 +46,7 @@ app.post("/productCreated", async (req, res) => {
   req.body.price = parseFloat(price);
   let product = { title: title, price: req.body.price, thumbnail: thumbnail, id: id };
 
-  res.render("productCreated.pug", { product: await container.save(product) });
+  res.render("productCreated.hbs", { product: await container.save(product) });
 
   res.status(200).json(req.body);
 
