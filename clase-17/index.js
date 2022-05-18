65210ac637cade3d2b7918c18d3e9bad23f30d11
Nahuel-DevOne <nahuel.profesor@gmail.com>
2022-05-17 21:03:17 -0300

    agregando código de la clase 17 con mongo

---
+++ clase-17/index.js
@@ -0,0 +1,20 @@
+/** NoSQL */
+
+//  productos
+
+//colección
+{
+    Id: 1;
+    nombre: "Leche";
+    desc: "Leche de vaca";
+    precio: 2.5;
+}
+
+{
+    id: 2;
+    nombre: "Huevos";
+    desc: "Huevos de gallina";
+    precio: 1.5;
+}
+
+
