b47de0f0e5add429d1f6e8e6e552eaddd7e09a5b
Nahuel-DevOne <nahuel.profesor@gmail.com>
2022-06-05 21:50:27 -0300

    segunda entrega del proyecto terminada

---
+++ desafio10-entrega2/app/config/config.js
@@ -0,0 +1,5 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./ecommerce-backend-f9b20-firebase-adminsdk-jh3ia-9c5d4601b4.json");
+
+module.exports = {admin, serviceAccount}
